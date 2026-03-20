90df2c20fe44435e644b6f4bbc32564dd5a2c9b9
Pranav ram <112083879+Pranavram22@users.noreply.github.com>
2026-03-20 15:47:36 +0530

    Add files via upload

---
+++ ServeltExp1/src/main/java/com/test/Lifecycle.java
@@ -0,0 +1,36 @@
+package com.test;
+
+import jakarta.servlet.GenericServlet;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+
+
+@WebServlet("/Lifecycle")
+public class Lifecycle extends GenericServlet {
+	private static final long serialVersionUID = 1L;
+
+    
+   
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("init()");
+	}
+
+	
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("destroy()");
+	}
+
+	
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("service() to process request & response");
+	}
+
+}
